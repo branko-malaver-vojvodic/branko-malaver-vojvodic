8f7ba711b6a6611373d636c535a84d2e49afc835
branko-malaver-vojvodic <42125251+branko-malaver-vojvodic@users.noreply.github.com>
2020-01-01 20:29:41 -0500

    Update send_form_email.php
    ewrtew

--- send_form_email.php
+++ send_form_email.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
+    $email_to = "bmala060@uottawa.ca";
     $email_subject = "Your email subject line";
  
     function died($error) {
